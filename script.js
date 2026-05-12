8ff59216049618ea6c7f351b32d6147bfdff3b47
Mahira Saleem <mahirasaleemahmad@gmail.com>
2026-05-12 15:58:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Mahira")
+
+var x=13, y=42, z=61;
+
+// legal variables name 
+
+var mahira = 23;
+
+var Miss_Naddiya = 10;
+
+var num1 = 20;
+
+var $100 = 30;
+
+var midTown = 50;
+
+var $dollar_currency = 80;
+
+// illegal variable name 
+
+// var 1no = 10 
+
+// var 100 = 80;
+
+// var my course = 54;
+
+// var var = 87
+
+// var gid vid sid = 876;
+
+// var num++ = 65;
+
+
+
+
